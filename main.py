aa66578c13b256980abd4cd8596d17aa668e2792
taraskz <158589585+taraskz@users.noreply.github.com>
2024-03-12 15:20:48 +0000

    Added a dicription if the game

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 ##############################################################################
-# Title: Marvel Superheroes - Part 4
+# Title: RPG Saving the Kingdom Adventure text game
 # Class: CS 30
-# Date: February 27, 2024
+# Date: March 12, 2024
 # Coders: Taras K
-# Version: 004
+# Version: 001
 ##############################################################################
+'''This program is a text based game where the user gets to be a hero and move
+    around a castle in order to eventually kill the evil king and save everyone
+    from his control
+    '''
 
